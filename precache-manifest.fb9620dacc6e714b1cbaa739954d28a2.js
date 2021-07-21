73958c59502ea55dd4e580f5b97b9b874da4d34c
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-21 14:18:21 +0300

    Updates

---
+++ precache-manifest.fb9620dacc6e714b1cbaa739954d28a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c530ca8749947d760dd63b74b6d1f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c46351b4e79e7a2ea88e",
+    "url": "./static/css/main.bb4cdaf3.chunk.css"
+  },
+  {
+    "revision": "5403c363955db0a2b973",
+    "url": "./static/js/2.771edc6b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.771edc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46351b4e79e7a2ea88e",
+    "url": "./static/js/main.bb6f8638.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
